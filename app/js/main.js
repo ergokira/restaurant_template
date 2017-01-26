4afd145b8a4acd0079cd555f8234495275df69af
ergokira <josuegb15@gmail.com>
2017-01-25 23:05:59 -0600

    header base commit

---
+++ app/js/main.js
@@ -0,0 +1,13 @@
+
+$(document).ready(function(){
+    "use strict"; // Start of use strict
+
+	//Script to Activate the Carousel
+
+	$('.carousel').carousel({
+		interval: 3000, //changes the speed
+		pause:null  //If set to "hover", pauses the cycling of the carousel on mouseenter and resumes the cycling of the carousel on mouseleave. If set to null, hovering over the carousel won't pause it.
+	})
+	
+   
+}); // End of use strict
