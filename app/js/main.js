3c08a7c8bf57e9a5af87339ef5c886bf2e5b2e45
ergokira <josuegb15@gmail.com>
2017-01-30 19:45:06 -0600

    About finished

--- app/js/main.js
+++ app/js/main.js
@@ -13,8 +13,8 @@ $(document).ready(function(){
 
 	//Script to Activate the Carousel
 	$('.carousel').carousel({
-		interval: 3000, //changes the speed
-		pause:null  //If set to "hover", pauses the cycling of the carousel on mouseenter and resumes the cycling of the carousel on mouseleave. If set to null, hovering over the carousel won't pause it.
+		interval: 3000, // Changes the speed
+		pause:null  // If set to "hover", pauses the cycling of the carousel on mouseenter and resumes the cycling of the carousel on mouseleave. If set to null, hovering over the carousel won't pause it.
 	})
 
 	// Offset for Main Navigation
