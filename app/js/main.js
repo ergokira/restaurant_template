253453d953eb047e31680e045a3f802941ceb0d4
ergokira <josuegb15@gmail.com>
2017-02-12 03:18:04 -0600

    Scroll Spy and hash routing integration

--- app/js/main.js
+++ app/js/main.js
@@ -2,20 +2,26 @@
 $(document).ready(function(){
     "use strict"; // Start of use strict
 
-	// Closes the Responsive Menu on Menu Item Click
+	//Hide mobile menu on click
 	$('.navbar-collapse ul li a').on('click', function(event) {
-		if (window.matchMedia("(max-width: 768px)").matches) {
+		if (window.matchMedia("(max-width: 760px)").matches) {
 			$(this).closest('.collapse').collapse('toggle');
 		}
-		$('.navbar-nav li').removeClass('active');
-		$(this).parent().addClass('active');
 	});
+
+	//Update the active section
+	$(window).on('load hashchange', function() {
+		var bodyLocation = $('body').scrollTop();
+		location.hash === '#contact' ? bodyLocation+=65 : bodyLocation-=65;
+		$('body').scrollTop(bodyLocation);
+	});
+	$('body').scrollspy({target: ".navbar", offset: 180}); 
 
 	//Script to Activate the Carousel
 	$('.carousel').carousel({
 		interval: 5000, // Changes the speed
 		pause:null  // If set to "hover", pauses the cycling of the carousel on mouseenter and resumes the cycling of the carousel on mouseleave. If set to null, hovering over the carousel won't pause it.
-	})
+	});
 
 	// Offset for Main Navigation
     $('#header-main-nav').affix({
